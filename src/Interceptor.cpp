406cd70a068fbbd0b807501f0b44168b8df869e3
Hugo Leclerc <hugal.leclerc@gmail.com>
2017-12-20 14:08:57 +0100

    sub

---
+++ src/Interceptor.cpp
@@ -0,0 +1,6 @@
+#include "Interceptor.h"
+
+Interceptor::Interceptor()
+{
+
+}
---
+++ src/Interceptor.h
@@ -0,0 +1,11 @@
+#ifndef INTERCEPTOR_H
+#define INTERCEPTOR_H
+
+
+class Interceptor
+{
+public:
+    Interceptor();
+};
+
+#endif // INTERCEPTOR_H
